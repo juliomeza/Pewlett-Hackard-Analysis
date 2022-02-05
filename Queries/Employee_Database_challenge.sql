e330b14c828d5414130747f5ef5d52aea12eb2b7
Julio Meza <juliomeza@gmail.com>
2022-02-05 07:57:23 -0500

    Challenge7 Deliverable1 DONE

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,3 +17,17 @@ FROM employees e
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no
 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, from_date DESC;
+
+-- Retiring Titles
+SELECT COUNT (title) count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC
+
